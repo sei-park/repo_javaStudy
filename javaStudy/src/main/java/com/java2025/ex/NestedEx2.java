756202a9b0b49fa6df84ce78c43b7c84e22e1ca8
sei-park <tpdlqkr@naver.com>
2025-10-05 22:49:45 +0900

    1005

--- javaStudy/src/main/java/com/java2025/ex/NestedEx2.java
+++ javaStudy/src/main/java/com/java2025/ex/NestedEx2.java
@@ -4,15 +4,14 @@ public class NestedEx2 {
 	
 	public static void main(String[] args) {
 		
-		int rows = 2;
-		
-		for(int i = 1; i <= 2; i++) {
-			
+		int rows = 5;   
+		 
+		for(int i = 1; i <= rows; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		
-		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-		
-		
 	}
 
 }
