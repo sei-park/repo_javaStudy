49c29b3246a7cc2f52fae9a21ccd51e3ba0d2068
sei-park <tpdlqkr@naver.com>
2025-10-06 00:15:16 +0900

    1006

---
+++ javaStudy/src/main/java/com/java2025/scanner/Scanner3.java
@@ -0,0 +1,26 @@
+package com.java2025.scanner;
+
+import java.util.Scanner;
+
+public class Scanner3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("첫 번째 숫자 입력: ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("두 번째 숫자 입력: ");
+		int num2 = scanner.nextInt();
+		
+		if(num1 > num2) {
+			System.out.println(num1);
+		} else if(num2 > num1) {
+			System.out.println(num2);
+		} else if(num1 == num2) {
+			System.out.println("두 숫자가 같음");
+		}
+	}
+
+}
