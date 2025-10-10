2ad41a04277dc24c5dbe91e7341a7b4fd2bcc009
sei-park <tpdlqkr@naver.com>
2025-10-10 18:37:22 +0900

    1010

---
+++ javaStudy/src/main/java/com/java2025/ex/ScannerEx3.java
@@ -0,0 +1,15 @@
+package com.java2025.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
