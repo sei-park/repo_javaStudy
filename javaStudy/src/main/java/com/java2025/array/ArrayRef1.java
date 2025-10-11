95362159bf0375b0d61451e50ebdb973451d96d5
sei-park <tpdlqkr@naver.com>
2025-10-11 23:03:05 +0900

    1011-3

---
+++ javaStudy/src/main/java/com/java2025/array/ArrayRef1.java
@@ -0,0 +1,28 @@
+package com.java2025.array;
+
+public class ArrayRef1 {
+	
+	public static void main(String[] args) {
+		
+		int[] students; // 배열 변수 선언
+		students = new int[5]; // 배열 생성
+		
+		// 변수 값 대입
+		students[0] = 90;
+		students[1] = 80;
+		students[2] = 70;
+		students[3] = 60;
+		students[4] = 50;
+		
+		// 사용
+		System.out.println(students[0]);
+		System.out.println(students[1]);
+		System.out.println(students[2]);
+		System.out.println(students[3]);
+		System.out.println(students[4]);
+		
+		
+		
+	}
+
+}
