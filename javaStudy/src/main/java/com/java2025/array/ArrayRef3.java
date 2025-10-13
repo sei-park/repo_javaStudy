f7964a7b69fa5f51fea81a5118e601ef8ebbc84a
sei-park <tpdlqkr@naver.com>
2025-10-13 20:16:04 +0900

    1013

---
+++ javaStudy/src/main/java/com/java2025/array/ArrayRef3.java
@@ -0,0 +1,25 @@
+package com.java2025.array;
+
+public class ArrayRef3 {
+	
+	public static void main(String[] args) {
+		
+		// 방법 1
+		// int[] students;
+ 		// students = new int[]{90, 80, 70, 60, 50};
+ 		
+ 		// 방법 2
+ 		// int[] students = new int[]{90, 80, 70, 60, 50};
+		
+		// 방법 3
+		int[] students = {90, 80, 70, 60, 50};
+			 
+		// 사용
+		for(int i = 0; i < 5; i++) {
+			System.out.println(students[i]); 
+		}
+	
+			
+	}
+
+}
