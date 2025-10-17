bc94fc735dde56d6ff238b383733ae20fbaef66b
sei-park <tpdlqkr@naver.com>
2025-10-17 21:19:28 +0900

    1017

--- javaStudy/src/main/java/com/java2025/ex/ProductAdminEx.java
+++ javaStudy/src/main/java/com/java2025/ex/ProductAdminEx.java
@@ -18,6 +18,11 @@ public class ProductAdminEx {
 			scanner.nextLine();
 			
 			if(menu == 1) {
+				if(productCount >= maxProducts) {
+					System.out.println("더 이상 상품을 등록할 수 없습니다.");
+					continue; // 다시 while문의 처음으로 돌아감
+				}
+				
 				System.out.print("상품 이름 입력: ");
 				productNames[productCount] = scanner.nextLine();
 				
@@ -25,6 +30,19 @@ public class ProductAdminEx {
 				productPrices[productCount] = scanner.nextInt();
 				
 				productCount++; // 상품 이름과 가겨을 입력 받은 후 카운트를 증가시킴
+			} else if(menu == 2) {
+				if(productCount == 0) {
+					System.out.println("등록된 상품이 없습니다.");
+					continue;
+				} 
+				for(int i = 0; i < productCount; i++) {
+					System.out.println(productNames[i] + ": " + productPrices[i] + "원");
+				}
+			} else if(menu == 3) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else {
+				System.out.println("잘못된 메뉴를 선택하셨습니다.");
 			}
 		}
 		
