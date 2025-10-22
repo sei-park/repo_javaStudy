23c1945c47d75a4a6a6667221ed1e4f71f978f9e
sei-park <tpdlqkr@naver.com>
2025-10-22 22:57:13 +0900

    1022-2

---
+++ javaStudy/src/main/java/com/java2025/ex/MethodEx2Ref.java
@@ -0,0 +1,20 @@
+package com.java2025.ex;
+
+public class MethodEx2Ref {
+	
+	public static void main(String[] args) {
+		String message = "Hello World";
+		printMessage(message , 3);
+		printMessage(message , 5);
+		printMessage(message , 7);
+		
+	}
+	
+	public static void printMessage(String message, int times) {
+		for(int i = 0; i < times; i++) {
+			System.out.println(message);
+		}
+	}
+	
+
+}
