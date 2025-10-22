413e30c561ab59597ef890a1ebe69533e3f2c978
sei-park <tpdlqkr@naver.com>
2025-10-22 22:11:15 +0900

    1022

--- javaStudy/src/main/java/com/java2025/method/Overloading1.java
+++ javaStudy/src/main/java/com/java2025/method/Overloading1.java
@@ -2,6 +2,15 @@ package com.java2025.method;
 
 public class Overloading1 {
 	
+	//	오버로딩 
+	//	- 이름이 같고 매개변수가 다른 매서드를 여러 개 정의 하는 것
+	//	- 매서드의 이름이 같아도 매개변수의 타입 및 순서가 다르면 오버로딩 할 수 있음
+	//	- 반환 타입은 인정하지 않음
+	
+	//매서드 시그니처 
+	//- 매서드를 구분할 수 있는 고유한 식별자나 서명
+	//- 매서드 이름이 같아도 매서드 시그니처가 다르면 다른 매서드로 간주
+	
 	public static void main(String[] args) {
 		System.out.println("1: " + add(1, 2));
 		System.out.println("2: " + add(1, 2, 3));
